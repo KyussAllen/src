c1ddf9e965bb3a06ddbe9d3596b311a0fbe3c293
kyuss <kyuss12@icloud.com>
2024-01-30 02:51:04 -0800

    #On branch main

---
+++ main.ts
@@ -0,0 +1,13 @@
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
